b17ac5e39faa44e9a06c9aebbaa6372bcc82da2e
Marek Šmarda <135655698+MarekSmarda001@users.noreply.github.com>
2025-05-08 23:13:29 +0200

    vite fix for virtualhost

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        host: 'localhost', // nebo 127.0.0.1
+        port: 5174,
+        origin: 'http://localhost:5174',
+        cors: {
+            origin: 'http://cms.local',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'X-Requested-With'],
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
